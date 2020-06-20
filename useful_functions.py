93cdcc7d496335861eb2bec8f9e4316042aa63e8
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-06-20 13:27:04 +0530

    created useful_functions.py

---
+++ useful_functions.py
@@ -0,0 +1,30 @@
+import json
+
+def computeMD5hash(my_string):
+    import hashlib
+    m = hashlib.md5()
+    m.update(my_string.encode('utf-8'))
+    return m.hexdigest()
+
+
+def write_to_file(this_file):
+    """[This takes a data and writes to a file]
+
+    Args:
+        this_file ([date]) : [writes to json file]
+    """
+    f = open('response_text.json', 'w')
+    f.write(this_file)
+
+
+
+def convertJSON():
+    """
+        This takes no arguments and returns a dictonary from json 
+    Returns:
+        [dict] : [from json file]
+    """
+    with open('response_text.json', 'r') as f:
+        distros_dict = json.load(f)
+    return distros_dict
+
